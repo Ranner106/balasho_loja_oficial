b96c13f6cb98e57888477053a5bfbd40c6c7c9ec
rannerdepaula <rannerdepaula@gmail.com>
2024-07-18 18:11:42 -0300

    configurando tailwind css

---
+++ tailwind.config.js
@@ -0,0 +1,15 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    './layout/*.liquid',
+    './sections/*.liquid',
+    './snippets/*.liquid',
+    './templates/customers/*.liquid',
+    './templates/customers/*.json',
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
+
